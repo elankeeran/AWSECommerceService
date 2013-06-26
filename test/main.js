0816d2f3f55b622de8e870f3ff236c0446148570
Elankeeran <ekeeran@gmail.com>
2013-06-26 19:12:05 +0530

    add generateSignature and getSignedUrl method

--- test/main.js
+++ test/main.js
@@ -1,13 +1,19 @@
-// test/main.js
+"use strict";
 var should = require('should');
 var AWSECommerceService = require('../lib/main');
 
 describe('AWSECommerceService', function() {
-    describe('with no arguments', function() {
-        it('returns an empty array', function() {
-            var result = AWSECommerceService();
-            result.should.eql([]);
+	 	var result = new AWSECommerceService({
+        	publicKey : 'xxxxxx',
+        	privateKey : 'aaaaas',
+        	affiliateTag : 'affiliateTag'
         });
-    });
-});
 
+        it('base URL webservices.amazon.com', function() {
+           result.baseURL.host.should.equal("webservices.amazon.com");
+        });
+
+        it('method should be GET' , function(){
+        	 result.baseURL.method.should.equal("GET");
+        });
+});
